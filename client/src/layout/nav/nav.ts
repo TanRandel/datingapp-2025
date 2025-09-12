ec21d757886917a311e602174e5d9993ada93aaf
Randel Joshua <randeljoshuatan@gmail.com>
2025-09-12 15:18:01 +0800

    End of section 5

---
+++ client/src/layout/nav/nav.ts
@@ -0,0 +1,37 @@
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountService } from '../../core/services/account-service';
+import { inject} from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  imports: [FormsModule],
+  templateUrl: './nav.html',
+  styleUrl: './nav.css'
+})
+
+export class Nav {
+
+  protected accountService = inject(AccountService)
+  protected creds: any = {}
+  //protected loggedIn = signal(false)
+
+  login(){
+    this.accountService.login(this.creds).subscribe({
+      next: result => {
+        console.log(result);
+        this.creds = {};
+      },
+      error:error => alert(error.message)
+      
+    })
+  }
+
+  logout(){
+    //this.loggedIn.set(false);
+    this.accountService.logout();
+  }
+  // login(){
+  //   console.log(this.creds);
+  // }
+}
